944464e82a93b48b837ea8d1dec4b4055075d35a
Charles Kubicek <c.kubicek@gmail.com>
2014-08-07 12:34:11 +0100

    YTA-187 Charles,Bruno, take modules out of .gitignore

---
+++ test/support/modules/SharedPageModules.scala
@@ -0,0 +1,60 @@
+package support.modules
+
+import org.openqa.selenium.support.ui.{ExpectedConditions, WebDriverWait}
+import org.openqa.selenium.{By, WebDriver}
+import org.scalatest.selenium.WebBrowser
+import support.page._
+import support.steps.Env
+
+trait SharedPageModules {
+
+
+  object BetaBanner extends WebBrowser {
+
+    implicit val webDriver: WebDriver = Env.driver
+
+    def feedbackLink = linkText("feedback")
+
+    def clickOnFeedbackLink() = {
+      click on feedbackLink
+      webDriver.switchTo().window(windowHandles.last)
+      new FeedbackPage
+    }
+  }
+
+  object GetHelpWithThisPage extends WebBrowser {
+
+    implicit val webDriver: WebDriver = Env.driver
+
+    def sectionQuery = className("report-error")
+
+    def nameField = textField("report-name")
+
+    def emailField = textField("report-email")
+
+    def whatWereYouDoingField = textField("report-action")
+
+    def whatDoYouNeedHelpWithField = textField("report-error")
+
+    def sendBtn = id("report-submit")
+
+    def ticketId = id("ticketId").element.attribute("value")
+
+    def sendProblemReport(name: String, email: String, whatWereYouDoing: String, whatDoYouNeedHelpWith: String): Unit = {
+      click on linkText("Get help with this page.")
+
+      nameField.value = name
+      emailField.value = email
+      whatWereYouDoingField.value = whatWereYouDoing
+      whatDoYouNeedHelpWithField.value = whatDoYouNeedHelpWith
+
+      click on sendBtn
+
+      val wait = new WebDriverWait(webDriver, 15)
+      wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("feedback-thank-you-header")))
+    }
+  }
+
+}
+
+
