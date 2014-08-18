b6b0739bf9fc74f90e1652e233ca369723394784
Bruno Aranda <bruno.aranda@digital.cabinet-office.gov.uk>
2014-08-18 17:22:27 +0100

    YTA-345 Added contact HMRC feature without javascript [Prashant/Bruno]

---
+++ test/features/ContactHmrc_NoJavascriptFeature.scala
@@ -0,0 +1,61 @@
+package features
+
+import org.skyscreamer.jsonassert.JSONCompareMode
+import support.page.ContactHmrcPage
+import support.steps.{ApiSteps, NavigationSteps, ObservationSteps}
+import support.stubs.{NoJsFeature, Login, StubbedFeature}
+
+class ContactHmrc_NoJavascriptFeature extends NoJsFeature with NavigationSteps with ApiSteps with ObservationSteps {
+
+  Feature("Contact HMRC with Javascript disabled") {
+
+    info("In order to make my views known")
+    info("As a Tax Payer with Javascript disabled in my browser")
+    info("I want to contact HMRC")
+
+
+    Background {
+      Given("I am logged in")
+
+      And("I go to the 'Contact HMRC' page")
+      go to new ContactHmrcPage
+      i_am_on_the_page("Contact HMRC")
+    }
+
+    Scenario("Contact form sent successfully") {
+      When("I fill the contact form correctly")
+      val page = new ContactHmrcPage
+      page.fillContactForm(Name, Email, Comment)
+
+      And("I send the contact form")
+      page.submitContactForm()
+
+      Then("I see:")
+      i_see("Thank you",
+        "Your message has been sent, and the team will get back to you within 2 working days.")
+
+      And("the Deskpro endpoint '/deskpro/ticket' has received the following POST request:")
+      verify_post(to = "/deskpro/ticket", body =
+        s"""
+          |{
+          |   "name":"$Name",
+          |   "email":"$Email",
+          |   "subject":"Contact form submission",
+          |   "message":"$Comment",
+          |   "referrer":"n/a",
+          |   "javascriptEnabled":"N",
+          |   "authId":"/auth/oid/1234567890",
+          |   "areaOfTax":"biztax",
+          |   "sessionId": "${Login.SessionId}",
+          |   "userTaxIdentifiers":{}
+          |}
+        """.stripMargin, JSONCompareMode.LENIENT)
+    }
+
+  }
+
+
+  private val Name = "Grumpy Bear"
+  private val Email = "grumpy@carebears.com"
+  private val Comment = "I am writing a comment"
+}
