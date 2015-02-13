81426b5dfbd14bc92123133be43f69d24bef2508
ramanallamilli <rama.nallamilli@gmail.com>
2015-02-13 11:20:08 +0000

    YTA-717: Deprecated usage of FrontEndRedirect class.

---
+++ app/controllers/ExternalUrls.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api.Play
+import uk.gov.hmrc.play.config.RunMode
+
+object ExternalUrls extends RunMode {
+  import play.api.Play.current
+
+  val ytaHost          = Play.configuration.getString(s"govuk-tax.$env.yta.host").getOrElse("")
+  val businessTaxHome = s"$ytaHost/account"
+}
