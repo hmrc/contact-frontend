f8ffe54912eceddc8cd9a6d24d02fde2be5d5009
petrnejedly <pnejedly@equalexperts.com>
2015-03-06 12:49:06 +0000

    YTA-782 upgraded to the latest play-frontend

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -10,11 +10,9 @@ object FrontendBuild extends Build with MicroService {
 }
 
 private object AppDependencies {
-  import play.PlayImport._
-  import play.core.PlayVersion
 
   private val playHealthVersion = "0.7.0"
-  private val playFrontendVersion =  "14.1.0"
+  private val playFrontendVersion =  "14.2.1"
   private val playUiVersion = "1.5.0"
   private val govUkTemplateVersion =  "2.1.0"
 
@@ -22,16 +20,11 @@ private object AppDependencies {
   private val pegdownVersion = "1.4.2"
 
   val compile = Seq(
-    ws,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-frontend" % playFrontendVersion,
     "uk.gov.hmrc" %% "play-ui" % playUiVersion,
     "uk.gov.hmrc" %% "govuk-template" % govUkTemplateVersion,
-    "uk.gov.hmrc" %% "url-builder" % "0.3.0",
-
-    "com.kenshoo" %% "metrics-play" % "2.3.0_0.1.6",
-    "com.codahale.metrics" % "metrics-graphite" % "3.0.1",
-    "org.apache.httpcomponents" % "httpclient" % "4.3.1"
+    "uk.gov.hmrc" %% "url-builder" % "0.3.0"
   )
 
   trait TestDependencies {
@@ -43,14 +36,13 @@ private object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "play-frontend" % playFrontendVersion % scope classifier "tests",
-        "org.scalatest" %% "scalatest" % "2.2.0" % scope,
+        "org.scalatest" %% "scalatest" % "2.2.2" % scope,
         "org.pegdown" % "pegdown" % "1.4.2" % scope,
-        "org.jsoup" % "jsoup" % "1.7.2" % scope,
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+        "org.jsoup" % "jsoup" % "1.7.3" % scope,
 
-        "com.github.tomakehurst" % "wiremock" % "1.46" % "test",
-        "uk.gov.hmrc" %% "scala-webdriver" % "4.17.0" % "test",
-        "uk.gov.hmrc" %% "hmrctest" % "0.1.0" % "test"
+        "com.github.tomakehurst" % "wiremock" % "1.48" % "test",
+        "uk.gov.hmrc" %% "scala-webdriver" % "4.18.0" % "test",
+        "uk.gov.hmrc" %% "hmrctest" % "0.3.0" % "test"
       )
     }.test
   }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,8 +10,10 @@ resolvers ++= Seq(
 
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.2")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.8")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.0.2")
+addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.1.0")
 
-addSbtPlugin("uk.gov.hmrc" % "hmrc-resolvers" % "0.2.0")
+addSbtPlugin("uk.gov.hmrc" % "hmrc-resolvers" % "0.4.0")
+
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
