fc4eb24d99b1bee664014c32a249348579a941eb
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2017-04-10 14:56:27 +0100

    library updates

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -25,14 +25,14 @@ private object AppOverrides{
 private object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.14.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.17.0",
     "uk.gov.hmrc" %% "play-authorised-frontend" % "6.3.0",
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
     "uk.gov.hmrc" %% "play-ui" % "7.0.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.1.0",
-    "uk.gov.hmrc" %% "url-builder" % "1.0.0",
+    "uk.gov.hmrc" %% "url-builder" % "2.1.0",
     "org.apache.httpcomponents" % "httpclient" % DependencyVersions.apacheHttpComponentsCore
   )
 
