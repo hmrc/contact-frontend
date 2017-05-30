1214675255fe02d1ae444c30ee9676a5086a55b4
Alen Mujezinovic <alen@owns.ch>
2017-05-30 14:17:36 +0100

    Use defs on config object

--- app/config/ConfigFrontendGlobal.scala
+++ app/config/ConfigFrontendGlobal.scala
@@ -2,26 +2,41 @@ package config
 
 import com.typesafe.config.Config
 import net.ceedubs.ficus.Ficus._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 import play.api.mvc._
 import play.api.{Application, Configuration, Play}
 import play.twirl.api.Html
 import uk.gov.hmrc.crypto.ApplicationCrypto
 import uk.gov.hmrc.play.audit.filters.FrontendAuditFilter
 import uk.gov.hmrc.play.config.{AppName, ControllerConfig, RunMode}
+import uk.gov.hmrc.play.filters.{CacheControlFilter, MicroserviceFilterSupport, RecoveryFilter}
+import uk.gov.hmrc.play.filters.frontend.HeadersFilter
 import uk.gov.hmrc.play.frontend.bootstrap.DefaultFrontendGlobal
+import uk.gov.hmrc.play.frontend.filters.SessionCookieCryptoFilter
 import uk.gov.hmrc.play.http.logging.filters.FrontendLoggingFilter
-import uk.gov.hmrc.play.filters.MicroserviceFilterSupport
 
-import play.api.i18n.Messages.Implicits._
-import play.api.Play.current
+object ContactFrontendGlobal
+  extends DefaultFrontendGlobal
+  with RunMode {
 
-object ContactFrontendGlobal extends DefaultFrontendGlobal with RunMode {
-
-  override val auditConnector = AuditConnector
-  override val loggingFilter = CFLoggingFilter
-  override val frontendAuditFilter = ContactFrontendAuditFilter
-  override lazy val frontendFilters = defaultFrontendFilters ++ Seq(CorsFilter)
-
+  override def auditConnector = AuditConnector
+  override def loggingFilter = CFLoggingFilter
+  override def frontendAuditFilter = ContactFrontendAuditFilter
+  override def frontendFilters = Seq(
+    metricsFilter,
+    HeadersFilter,
+    SessionCookieCryptoFilter,
+    deviceIdFilter,
+    loggingFilter,
+    frontendAuditFilter,
+    sessionTimeoutFilter,
+    csrfExceptionsFilter,
+    csrfFilter,
+    CacheControlFilter.fromConfig("caching.allowedContentTypes"),
+    RecoveryFilter,
+    CorsFilter
+  )
 
   override def onStart(app: Application) {
     super.onStart(app)
@@ -31,7 +46,8 @@ object ContactFrontendGlobal extends DefaultFrontendGlobal with RunMode {
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: Request[_]): Html =
     views.html.error_template(pageTitle, heading, message)
 
-  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"$env.microservice.metrics")
+  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] =
+    app.configuration.getConfig(s"$env.microservice.metrics")
 
 }
 
