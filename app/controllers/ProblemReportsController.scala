96f1ecbbc7cb942886e6d3f74ee34db3b5b61470
joanna.pintopaul <joanna.pintopaul@digital.hmrc.gov.uk>
2017-11-13 14:04:01 +0000

    PLATY-62: Updated Ajax response to include server side errors

--- app/controllers/ProblemReportsController.scala
+++ app/controllers/ProblemReportsController.scala
@@ -16,11 +16,8 @@ import util.DeskproEmailValidator
 
 import scala.concurrent.Future
 
-@Singleton
-class ProblemReportsController @Inject()(val hmrcDeskproConnector : HmrcDeskproConnector,
-                                         val authConnector : AuthConnector,
-                                         emailValidator: DeskproEmailValidator)(implicit  appConfig : AppConfig, override val messagesApi : MessagesApi) extends FrontendController with ContactFrontendActions with I18nSupport {
-
+object ProblemReportForm {
+  private val emailValidator: DeskproEmailValidator = DeskproEmailValidator()
   private val validateEmail: (String) => Boolean = emailValidator.validate
 
   val form = Form[ProblemReport](
@@ -43,18 +40,59 @@ class ProblemReportsController @Inject()(val hmrcDeskproConnector : HmrcDeskproC
       "referrer" -> optional(text)
     )(ProblemReport.apply)(ProblemReport.unapply)
   )
+}
+
+object ProblemReportFormV2 {
+
+  private val emailValidator: DeskproEmailValidator = DeskproEmailValidator()
+  private val validateEmail: (String) => Boolean = emailValidator.validate
+
+  val form = Form[ProblemReportV2](
+    mapping(
+      "report-name" -> text
+        .verifying("error.common.problem_report.action_mandatory", action => !action.isEmpty)
+        .verifying("error.common.problem_report.name_too_long", name => name.size <= 70)
+        .verifying("error.common.problem_report.name_invalid_characters", name => name.matches( """^[A-Za-z\-.,()'"\s]+$""")),
+      "report-email" -> text
+        .verifying("error.email", validateEmail)
+        .verifying("deskpro.email_too_long", email => email.size <= 255),
+      "report-action" -> text
+        .verifying("error.common.problem_report.action_mandatory", action => !action.isEmpty)
+        .verifying("error.common.comments_too_long", action => action.size <= 1000),
+      "report-error" -> text
+        .verifying("error.common.problem_report.action_mandatory", error => !error.isEmpty)
+        .verifying("error.common.comments_too_long", error => error.size <= 1000),
+      "isJavascript" -> boolean,
+      "service" -> optional(text),
+      "referrer" -> optional(text),
+      "csrfToken" -> optional(text),
+      "use-secure" -> boolean
+    )(ProblemReportV2.apply)(ProblemReportV2.unapply)
+  )
+}
+
+@Singleton
+class ProblemReportsController @Inject()(val hmrcDeskproConnector: HmrcDeskproConnector,
+                                         val authConnector: AuthConnector)(implicit appConfig: AppConfig, override val messagesApi: MessagesApi) extends FrontendController with ContactFrontendActions with I18nSupport {
 
   //TODO default to true (or even remove the secure query string) once everyone is off play-frontend so that we use the CSRF check (needs play-partials 1.3.0 and above in every frontend)
   def reportForm(secure: Option[Boolean], preferredCsrfToken: Option[String], service: Option[String]) = Action { implicit request =>
     val isSecure = secure.getOrElse(false)
-    val postEndpoint = if(isSecure) appConfig.externalReportProblemSecureUrl else appConfig.externalReportProblemUrl
-    val csrfToken = preferredCsrfToken.orElse { if(isSecure) Some("{{csrfToken}}") else None }
+    val postEndpoint = if (isSecure) appConfig.externalReportProblemSecureUrl else appConfig.externalReportProblemUrl
+    val csrfToken = preferredCsrfToken.orElse {
+      if (isSecure) Some("{{csrfToken}}") else None
+    }
 
     Ok(views.html.partials.error_feedback(postEndpoint, csrfToken, service))
   }
 
   def reportFormAjax(service: Option[String]) = UnauthorisedAction { implicit request =>
-    Ok(views.html.partials.error_feedback_inner(appConfig.externalReportProblemSecureUrl, None, service))
+    if (appConfig.useV2ProblemReportAjax) {
+      Ok(views.html.partials.error_feedback_inner_v2(ProblemReportFormV2.form, appConfig.externalReportProblemSecureUrl, None, service, true))
+    }
+    else {
+      Ok(views.html.partials.error_feedback_inner(appConfig.externalReportProblemSecureUrl, None, service))
+    }
   }
 
   def reportFormNonJavaScript(service: Option[String]) = UnauthorisedAction { implicit request =>
@@ -69,30 +107,79 @@ class ProblemReportsController @Inject()(val hmrcDeskproConnector : HmrcDeskproC
   }
 
   private[controllers] def doReport(thankYouMessage: Option[String] = None)(implicit request: Request[AnyRef]) = {
-    form.bindFromRequest.fold(
-      error => {
-        if (!error.data.getOrElse("isJavascript", "true").toBoolean) {
-          Future.successful(Ok(views.html.problem_reports_error_nonjavascript()))
-        } else {
-          Future.successful(BadRequest(Json.toJson(Map("status" -> "ERROR"))))
+    if (appConfig.useV2ProblemReportAjax) {
+      ProblemReportFormV2.form.bindFromRequest.fold(
+        (error: Form[ProblemReportV2]) => {
+          if (!error.data.getOrElse("isJavascript", "true").toBoolean) {
+            Future.successful(Ok(views.html.problem_reports_error_nonjavascript()))
+          } else {
+            val isSecure = error.data.get("is-secure").exists(_.toBoolean)
+            val postEndpoint = if (isSecure) appConfig.externalReportProblemSecureUrl else appConfig.externalReportProblemUrl
+            val csrfToken = error.data
+              .get("preferred-csrf-token")
+              .orElse {
+                if (isSecure) Some("{{csrfToken}}") else None
+              }
+            val errorHtmlAsString =
+              views.html.partials.error_feedback_inner_v2(error, postEndpoint, csrfToken, error.data.get("service"), isSecure).toString()
+            Future.successful(Ok(Json.toJson(Map("status" -> "OK", "message" -> errorHtmlAsString))))
+          }
+        },
+        problemReport => {
+          val referrer = if (problemReport.referrer.exists(_.trim.length > 0)) problemReport.referrer.get else referrerFrom(request)
+          (for {
+            maybeUserEnrolments <- maybeAuthenticatedUserEnrolments
+            ticketId <- createTicketV2(problemReport, request, maybeUserEnrolments, referrer)
+          } yield {
+            if (!problemReport.isJavascript) Ok(views.html.problem_reports_confirmation_nonjavascript(ticketId.ticket_id.toString, thankYouMessage))
+            else Ok(Json.toJson(Map("status" -> "OK", "message" -> views.html.ticket_created_body(ticketId.ticket_id.toString, thankYouMessage).toString())))
+          }) recover {
+            case _ if !problemReport.isJavascript => Ok(views.html.problem_reports_error_nonjavascript())
+          }
         }
-      },
-      problemReport => {
-        val referrer = if(problemReport.referrer.exists(_.trim.length > 0)) problemReport.referrer.get else referrerFrom(request)
-        (for {
-          maybeUserEnrolments <- maybeAuthenticatedUserEnrolments
-          ticketId <- createTicket(problemReport, request, maybeUserEnrolments, referrer)
-        } yield {
-          if (!problemReport.isJavascript) Ok(views.html.problem_reports_confirmation_nonjavascript(ticketId.ticket_id.toString, thankYouMessage))
-          else Ok(Json.toJson(Map("status" -> "OK", "message" -> views.html.ticket_created_body(ticketId.ticket_id.toString, thankYouMessage).toString())))
-        }) recover {
-          case _ if !problemReport.isJavascript => Ok(views.html.problem_reports_error_nonjavascript())
+      )
+    }
+    else {
+      ProblemReportForm.form.bindFromRequest.fold(
+        (error: Form[ProblemReport]) => {
+          if (!error.data.getOrElse("isJavascript", "true").toBoolean) {
+            Future.successful(Ok(views.html.problem_reports_error_nonjavascript()))
+          } else {
+            Future.successful(BadRequest(Json.toJson(Map("status" -> "ERROR"))))
+          }
+        },
+        problemReport => {
+          val referrer = if (problemReport.referrer.exists(_.trim.length > 0)) problemReport.referrer.get else referrerFrom(request)
+          (for {
+            maybeUserEnrolments <- maybeAuthenticatedUserEnrolments
+            ticketId <- createTicket(problemReport, request, maybeUserEnrolments, referrer)
+          } yield {
+            if (!problemReport.isJavascript) Ok(views.html.problem_reports_confirmation_nonjavascript(ticketId.ticket_id.toString, thankYouMessage))
+            else Ok(Json.toJson(Map("status" -> "OK", "message" -> views.html.ticket_created_body(ticketId.ticket_id.toString, thankYouMessage).toString())))
+          }) recover {
+            case _ if !problemReport.isJavascript => Ok(views.html.problem_reports_error_nonjavascript())
+          }
         }
-      }
-    )
+      )
+    }
   }
 
   private def createTicket(problemReport: ProblemReport, request: Request[AnyRef], enrolmentsOption: Option[Enrolments], referrer: String) = {
+    implicit val hc = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
+    hmrcDeskproConnector.createDeskProTicket(
+      name = problemReport.reportName,
+      email = problemReport.reportEmail,
+      subject = "Support Request",
+      message = problemMessage(problemReport.reportAction, problemReport.reportError),
+      referrer = referrer,
+      isJavascript = problemReport.isJavascript,
+      request = request,
+      enrolmentsOption = enrolmentsOption,
+      service = problemReport.service
+    )
+  }
+
+  private def createTicketV2(problemReport: ProblemReportV2, request: Request[AnyRef], enrolmentsOption: Option[Enrolments], referrer: String) = {
     implicit val hc = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
     hmrcDeskproConnector.createDeskProTicket(
       name = problemReport.reportName,
@@ -123,3 +210,5 @@ class ProblemReportsController @Inject()(val hmrcDeskproConnector : HmrcDeskproC
 }
 
 case class ProblemReport(reportName: String, reportEmail: String, reportAction: String, reportError: String, isJavascript: Boolean, service: Option[String], referrer: Option[String])
+
+case class ProblemReportV2(reportName: String, reportEmail: String, reportAction: String, reportError: String, isJavascript: Boolean, service: Option[String], referrer: Option[String], preferredCsrfToken: Option[String], useSecure: Boolean)
