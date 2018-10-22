bf6acf71a8eff14373349acf2dfc242c50767140
jamesluahmrc <james.lua@digital.hmrc.gov.uk>
2018-10-22 17:03:29 +0100

    PLATY-702: Add test to ensure A/B partitioning is fair

--- test/util/ABTestingFeaturePartitionerSpec.scala
+++ test/util/ABTestingFeaturePartitionerSpec.scala
@@ -6,7 +6,7 @@ class ABTestingFeaturePartitionerSpec extends WordSpec with Matchers{
   "GetHelpWithThisPageFeaturePartitioner" when {
     "partitioning" should {
       val testInstance =
-        new ABTestingFeaturePartitioner[Int, GetHelpWithThisPageFeature, GetHelpWithThisPageFeature_A, GetHelpWithThisPageFeature_B](50,
+        new ABTestingFeaturePartitioner[Any, GetHelpWithThisPageFeature, GetHelpWithThisPageFeature_A, GetHelpWithThisPageFeature_B](50,
           GetHelpWithThisPageFeature_A,
           GetHelpWithThisPageFeature_B)
 
@@ -32,6 +32,22 @@ class ABTestingFeaturePartitionerSpec extends WordSpec with Matchers{
 
       "return feature B, for negative hashable" in {
         testInstance.partition(-51) shouldBe GetHelpWithThisPageFeature_B
+      }
+
+      "return feature A and B in proportions within 2 standard deviations from expected probability (50%)" in {
+        def randomString: String = scala.util.Random.nextString(randomLengthBetween(1, 100))
+        def randomLengthBetween(minStringLength: Int, maxStringLength: Int): Int = {
+          minStringLength + scala.util.Random.nextInt(maxStringLength + 1 - minStringLength)
+        }
+
+        val testStringCount: Int = 1000000
+        val testStrings: Seq[String] = Seq.fill(testStringCount)(randomString)
+        val featureACount: Int = testStrings.count(testInstance.partition(_) == GetHelpWithThisPageFeature_A)
+
+        val difference: Double = Math.abs(0.5 * testStringCount - featureACount)
+        val twoStandardDeviations: Double = Math.sqrt(testStringCount)
+
+        difference should be <= twoStandardDeviations
       }
     }
   }
