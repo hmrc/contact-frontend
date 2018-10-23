17d9e71f7d8ee443c9a54f77ad01f75d4265f9e2
jamesluahmrc <james.lua@digital.hmrc.gov.uk>
2018-10-23 14:32:21 +0100

    PLATY-BAU: Add test for randomness of A/B partitioning

--- test/util/DeviceIdBucketCalculatorSpec.scala
+++ test/util/DeviceIdBucketCalculatorSpec.scala
@@ -3,7 +3,7 @@ package util
 import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{Matchers, WordSpec}
-import play.api.mvc.Cookie
+import play.api.mvc.{AnyContentAsEmpty, Cookie}
 import play.api.test.FakeRequest
 import uk.gov.hmrc.http.CookieNames
 
@@ -29,6 +29,52 @@ class DeviceIdBucketCalculatorSpec extends WordSpec with Matchers with PropertyC
     val requestWithoutDeviceId = FakeRequest.apply()
 
     BucketCalculator.deviceIdBucketCalculator(requestWithoutDeviceId) shouldBe 0
+  }
+
+
+  "Device ID should be assigned from 1 to 99 with roughly equal probability distribution (chi-squared 99% certainty for 1000 degrees of freedom)" in {
+    def generateTestRequests(deviceIdCount: Int,
+                             minDeviceIdPadLength: Int,
+                             maxDeviceIdPadLength: Int,
+                             validChars: String,
+                             deviceIdPrefix: String = "",
+                             deviceIdSuffix: String = ""): List[FakeRequest[AnyContentAsEmpty.type]] = {
+
+      def randomString(minLength: Int, maxLength: Int): String = {
+        def randomNumberBetween(min: Int, max: Int): Int = min + scala.util.Random.nextInt(max + 1 - min)
+        def randomChar: Char = validChars(randomNumberBetween(0, validChars.length - 1))
+        val stringBuilder = new StringBuilder
+        if (maxLength > 0) for (i <- 1 to randomNumberBetween(1, randomNumberBetween(minLength, maxLength))) stringBuilder.append(randomChar)
+        stringBuilder.toString()
+      }
+
+      val testDeviceIds: List[String] = List.fill(deviceIdCount)(s"$deviceIdPrefix${randomString(minDeviceIdPadLength, maxDeviceIdPadLength)}$deviceIdSuffix")
+      testDeviceIds.map(deviceId => FakeRequest.apply().withCookies(Cookie(CookieNames.deviceID, deviceId)))
+    }
+
+    val testDeviceIdCount: Int = 1001
+    val testRequests: List[FakeRequest[AnyContentAsEmpty.type]] =
+      generateTestRequests(
+        testDeviceIdCount,
+        minDeviceIdPadLength = 1,
+        maxDeviceIdPadLength = 100,
+        validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$%&'()*+-./:<=>?@[]^_`{|}~",
+        deviceIdPrefix = "mdtpi#",
+        deviceIdSuffix = "#blah"
+      )
+
+    val expectedBucketCount: Double = testDeviceIdCount/100
+    val intDistribution: List[(Int, Int)] = testRequests.map(BucketCalculator.deviceIdBucketCalculator(_)).groupBy(x => x).mapValues(_.size).toList
+    val squares: List[Double] = for {
+      (_, count) <- intDistribution
+      deviation = expectedBucketCount - count
+      square = deviation * deviation
+    } yield square
+
+    val chiSquaredNinetyNinePercentCertaintyBoundary: Double = 898.912
+    val pearsonStatistic: Double = squares.sum / expectedBucketCount
+
+    pearsonStatistic should be <= chiSquaredNinetyNinePercentCertaintyBoundary
   }
 
 }
