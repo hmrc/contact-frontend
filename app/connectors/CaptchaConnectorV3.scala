0f6ae7473a6ee6d555a9e33774f59683b0a551e4
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2020-10-15 09:40:21 +0100

    PLATUI-711: Uplifted dependencies

--- app/connectors/CaptchaConnectorV3.scala
+++ app/connectors/CaptchaConnectorV3.scala
@@ -8,8 +8,7 @@ package connectors
 import config.AppConfig
 import javax.inject.Inject
 import play.api.libs.json.{Json, _}
-import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -17,18 +16,21 @@ class CaptchaConnectorV3 @Inject()(http: HttpClient, appConfig: AppConfig)(impli
 
   def verifyResponse(userResponseToken: String)(implicit hc: HeaderCarrier): Future[CaptchaApiResponseV3] = {
 
-    http.POSTForm(
-      url = appConfig.captchaVerifyUrl,
-      body = Map(
-        "secret" -> Seq(appConfig.captchaServerKey),
-        "response" -> Seq(userResponseToken)
+    http
+      .POSTForm(
+        url = appConfig.captchaVerifyUrl,
+        body = Map(
+          "secret"   -> Seq(appConfig.captchaServerKey),
+          "response" -> Seq(userResponseToken)
+        )
       )
-    ).foreach(resp => println(resp.json))
+      .foreach(resp => println(resp.json))
 
+    import uk.gov.hmrc.http.HttpReads.Implicits._
     http.POSTForm[CaptchaApiResponseV3](
       url = appConfig.captchaVerifyUrl,
       body = Map(
-        "secret" -> Seq(appConfig.captchaServerKey),
+        "secret"   -> Seq(appConfig.captchaServerKey),
         "response" -> Seq(userResponseToken)
       )
     )
@@ -43,17 +45,16 @@ class CaptchaConnectorV3 @Inject()(http: HttpClient, appConfig: AppConfig)(impli
   * @param score - the score for this request (0.0 - 1.0)
   * @param action - the action name for this request (identifies e.g. individual form)
   */
-
 sealed trait CaptchaApiResponseV3
-case class SuccessfulCaptchaApiResponse(score : BigDecimal, action : String) extends CaptchaApiResponseV3
-case class UnsuccessfulCaptchaApiResponse(errorCodes : Seq[String]) extends CaptchaApiResponseV3
-
+case class SuccessfulCaptchaApiResponse(score: BigDecimal, action: String) extends CaptchaApiResponseV3
+case class UnsuccessfulCaptchaApiResponse(errorCodes: Seq[String]) extends CaptchaApiResponseV3
 
 object CaptchaApiResponseV3 {
 
   class ExplicitRuleFormat extends Reads[CaptchaApiResponseV3] {
 
-    val unsuccessfulReads: Reads[UnsuccessfulCaptchaApiResponse] = (JsPath \ "error-codes").read[List[String]].map(UnsuccessfulCaptchaApiResponse)
+    val unsuccessfulReads: Reads[UnsuccessfulCaptchaApiResponse] =
+      (JsPath \ "error-codes").read[List[String]].map(UnsuccessfulCaptchaApiResponse)
 
     override def reads(json: JsValue): JsResult[CaptchaApiResponseV3] = {
       val success = (json \ "success").as[Boolean]
--- app/connectors/deskpro/HmrcDeskproConnector.scala
+++ app/connectors/deskpro/HmrcDeskproConnector.scala
@@ -9,15 +9,14 @@ import connectors.deskpro.domain.{Feedback, Ticket, TicketId}
 import javax.inject.Inject
 import play.api.mvc.Request
 import uk.gov.hmrc.auth.core.Enrolments
-import uk.gov.hmrc.http.{HeaderCarrier, NotFoundException, Upstream5xxResponse}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, NotFoundException, UpstreamErrorResponse}
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
-import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.http.HttpReads.Implicits._
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class HmrcDeskproConnector @Inject()(http: HttpClient,
-                                     servicesConfig: ServicesConfig)
-                                    (implicit executionContext: ExecutionContext) {
+class HmrcDeskproConnector @Inject()(http: HttpClient, servicesConfig: ServicesConfig)(
+  implicit executionContext: ExecutionContext) {
 
   def serviceUrl: String = servicesConfig.baseUrl("hmrc-deskpro")
 
@@ -34,10 +33,21 @@ class HmrcDeskproConnector @Inject()(http: HttpClient,
     abFeatures: Option[String],
     userAction: Option[String])(implicit hc: HeaderCarrier): Future[TicketId] = {
     val ticket = Ticket
-      .create(name, email, subject, message, referrer, isJavascript, hc, request, enrolmentsOption, service, abFeatures,
+      .create(
+        name,
+        email,
+        subject,
+        message,
+        referrer,
+        isJavascript,
+        hc,
+        request,
+        enrolmentsOption,
+        service,
+        abFeatures,
         userAction)
     http.POST[Ticket, TicketId](requestUrl("/deskpro/get-help-ticket"), ticket) recover {
-      case nf: NotFoundException => throw new Upstream5xxResponse(nf.getMessage, 404, 500)
+      case nf: NotFoundException => throw UpstreamErrorResponse(nf.getMessage, 404, 500)
     }
   }
 
@@ -69,7 +79,7 @@ class HmrcDeskproConnector @Inject()(http: HttpClient,
         service,
         abFeatures)
     ) recover {
-      case nf: NotFoundException => throw new Upstream5xxResponse(nf.getMessage, 404, 500)
+      case nf: NotFoundException => throw UpstreamErrorResponse(nf.getMessage, 404, 500)
     }
 
   private def requestUrl[B, A](uri: String): String = s"$serviceUrl$uri"
