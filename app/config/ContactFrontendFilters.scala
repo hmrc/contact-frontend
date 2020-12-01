7e0acb5d2e93632fc77e2a53f579ad0fdb5b9df9
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-12-01 13:33:15 +0000

    PLATUI-793: Enable CSPFilter

--- app/config/ContactFrontendFilters.scala
+++ app/config/ContactFrontendFilters.scala
@@ -8,7 +8,11 @@ package config;
 import javax.inject.Inject
 import play.api.http.DefaultHttpFilters
 import play.filters.cors.CORSFilter
+import play.filters.csp.CSPFilter
 import uk.gov.hmrc.play.bootstrap.frontend.filters.FrontendFilters
 
-class ContactFrontendFilters @Inject() (defaultFilters: FrontendFilters, playCORSFilter: CORSFilter)
-    extends DefaultHttpFilters(defaultFilters.filters :+ playCORSFilter: _*)
+class ContactFrontendFilters @Inject() (
+  defaultFilters: FrontendFilters,
+  playCORSFilter: CORSFilter,
+  cspFilter: CSPFilter
+) extends DefaultHttpFilters(cspFilter +: defaultFilters.filters :+ playCORSFilter: _*)
