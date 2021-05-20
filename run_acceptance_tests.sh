32c005caa2820edc552d84a457756bfd268a8387
Nat Carey <nat@dorightdigital.com>
2021-05-20 15:07:29 +0100

    Simplifying kick-off script.

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 ENV=${1:-local}
 BROWSER=${2:-chrome}
-DRIVER=
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
-fi
+sbt \
+  -Dbrowser=$BROWSER \
+  -Denvironment=$ENV \
+  -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver \
+  -Dwebdriver.gecko.driver=/usr/local/bin/geckodriver \
+  acceptance:test
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER acceptance:test
