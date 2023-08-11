23aa59d76534c3a7bb2ff45c1913bfc4f03bff15
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2023-08-11 13:03:15 +0100

    PLATUI-BAU: Update acceptance test scripts

---
+++ run-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+BROWSER=$1
+ENVIRONMENT=$2
+
+sbt scalafmtCheckAll scalafmtSbtCheck
+
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" acceptance:test
