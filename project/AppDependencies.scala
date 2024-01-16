ed37b2edc6556708a61b05467f0ea2433e5b4269
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-01-16 10:47:04 +0000

    PLATUI-2716: Test branch for Play 2.9 upgrade to play-ui for smoke testing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,24 +7,22 @@ object AppDependencies {
     compile ++ testDependencies
   }
 
-  private val bootstrapPlayVersion = "7.22.0"
+  private val bootstrapFrontendVersion = "8.1.0"
+  private val playFrontendHmrcVersion  = "8.0.0"
+  private val playVersion              = "play-29"
 
   private val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.29.0-play-28",
-    "uk.gov.hmrc" %% "govuk-template"             % "5.80.0-play-28",
-    "uk.gov.hmrc" %% "play-ui"                    % "9.12.0-play-28"
+    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion" % bootstrapFrontendVersion,
+    "uk.gov.hmrc" %% s"play-frontend-hmrc-$playVersion" % playFrontendHmrcVersion,
+    "uk.gov.hmrc" %% "govuk-template"                   % "5.80.0-play-28",
+    "uk.gov.hmrc" %% s"play-ui-$playVersion"            % "10.0.0-SNAPSHOT"
   )
 
   private def test(scope: String) = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-test-play-28"  % bootstrapPlayVersion % scope,
-    "uk.gov.hmrc"           %% "domain"                  % "8.3.0-play-28"      % scope,
-    "uk.gov.hmrc"           %% "webdriver-factory"       % "0.41.0"             % scope,
-    "org.scalatestplus"     %% "scalatestplus-mockito"   % "1.0.0-M2"           % scope,
-    "org.scalatestplus"     %% "selenium-4-2"            % "3.2.13.0"           % scope,
-    "org.mockito"           %% "mockito-scala-scalatest" % "1.16.37"            % scope,
-    "org.jsoup"              % "jsoup"                   % "1.11.3"             % scope,
-    "com.github.tomakehurst" % "wiremock"                % "1.58"               % scope,
-    "com.github.tomakehurst" % "wiremock-jre8"           % "2.27.2"             % scope
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion" % bootstrapFrontendVersion % scope,
+    "uk.gov.hmrc"       %% "webdriver-factory"            % "0.46.0"                 % scope,
+    "org.scalatestplus" %% "selenium-4-2"                 % "3.2.13.0"               % scope,
+    "org.mockito"       %% "mockito-scala-scalatest"      % "1.16.37"                % scope,
+    "org.jsoup"          % "jsoup"                        % "1.11.3"                 % scope
   )
 }
