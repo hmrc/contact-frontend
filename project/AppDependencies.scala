cd3e8892791a373689938651bf8c206012ec0717
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-08-15 10:36:29 +0100

    NOJIRA: Bump play-frontend-hmrc versions (#305)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   }
 
   private val bootstrapFrontendVersion = "9.0.0"
-  private val playFrontendHmrcVersion  = "10.6.0"
+  private val playFrontendHmrcVersion  = "10.8.0"
   private val playVersion              = "play-30"
 
   private val compile = Seq(
