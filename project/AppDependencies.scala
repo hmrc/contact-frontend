c2cb217a67b08551b6f13053a8ac92159aa330c6
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2025-03-20 15:14:18 +0000

    PLATUI-3538: Updated frontend library (#321)
    PLATUI-3538: Updated frontend library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapFrontendVersion = "9.11.0"
-  private val playFrontendHmrcVersion  = "11.13.0"
+  private val playFrontendHmrcVersion  = "12.0.0"
   private val playVersion              = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
