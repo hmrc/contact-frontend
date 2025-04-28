6f4ce4fc3bea7d33ee577536145290b71dee9979
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2025-04-28 08:59:36 +0100

    PLATUI-3629 Example usage of service navigation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapFrontendVersion = "9.11.0"
-  private val playFrontendHmrcVersion  = "12.0.0"
+  private val playFrontendHmrcVersion  = "12.0.0-SNAPSHOT"
   private val playVersion              = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
