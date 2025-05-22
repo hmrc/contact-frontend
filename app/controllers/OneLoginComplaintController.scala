d1aa51b77da85142e64da82d2d3d3be6720aa9fb
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2025-05-22 08:57:48 +0100

    PLATUI-3699 Scala formatter

--- app/controllers/OneLoginComplaintController.scala
+++ app/controllers/OneLoginComplaintController.scala
@@ -35,7 +35,7 @@ import scala.concurrent.{ExecutionContext, Future}
 object OneLoginComplaintFormBind {
   private val emailValidator = DeskproEmailValidator()
 
-  private val nameValidator  = NameValidator()
+  private val nameValidator = NameValidator()
 
   def form: Form[OneLoginComplaintForm] = Form[OneLoginComplaintForm](
     mapping(
