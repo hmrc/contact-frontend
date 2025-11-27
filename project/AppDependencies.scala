6bf0b5c023cc5258670d57416b82ef4adf06d7f0
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2025-11-27 10:19:23 +0000

    PLATUI-4031: Bumped dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapFrontendVersion = "10.3.0"
+  private val bootstrapFrontendVersion = "10.4.0"
   private val playFrontendHmrcVersion  = "12.22.0"
   private val playVersion              = "play-30"
 
